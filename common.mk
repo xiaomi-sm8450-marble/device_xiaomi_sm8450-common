5d4f873383a6da0e1dc51bf416d4b03bd3d4612f
Mathieu Chartier <mathieuc@google.com>
2024-04-16 12:02:34 +0200

    sm8450-common: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- common.mk
+++ common.mk
@@ -463,6 +463,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
