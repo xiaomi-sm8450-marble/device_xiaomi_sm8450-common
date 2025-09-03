bdbf150666824277451457d05c7da2cef77e2c74
AdarshGrewal <AdarshGrewal@users.noreply.github.com>
2025-09-03 09:56:17 +0530

    sm8450-common: Switch to AOSP WFD

--- extract-files.py
+++ extract-files.py
@@ -43,7 +43,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.qccvndhal@1.0',
         'vendor.qti.imsrtpservice@3.0',
         'vendor.qti.diaghal@1.0',
-        'vendor.qti.hardware.wifidisplaysession@1.0',
         'com.qualcomm.qti.dpm.api@1.0',
     ): lib_fixup_vendor_suffix,
     (
